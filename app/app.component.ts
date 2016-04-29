828dd5cd33e95603cd3209e1a7afc3128e62738b
Robert Ellis <robertellis.rob@gmail.com>
2016-04-29 01:34:58 -0600

    I Fixed the issue. Thanks Angular2 Hero Tutorial writers for switching *ngFor="#hero for heroes" to *ngHero="let hero for heroes" and breaking everything :(

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,12 +9,12 @@ import {HeroService} from './hero.service';
     <h1>{{title}}</h1>
     <h2>My Heroes</h2>
     <ul class="heroes">
-      <li *ngFor="let hero of heroes"
+      <li *ngFor="#hero of heroes"
         [class.selected]="hero === selectedHero"
         (click)="onSelect(hero)">
         <span class="badge">{{hero.id}}</span> {{hero.name}}
       </li>
-    </ul>
+    </ul>   
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
   styles:[`
